77823ded0291949afc69781cca7000d1faa1e6f1
Sam Robiner <smr276@cornell.edu>
2018-10-13 11:39:42 -0400

    Bigram probs for tags

--- hmm.py
+++ hmm.py
@@ -5,11 +5,43 @@ import math
 import csv
 import collections
 
+### CONSTANTS ###
+O_XXX = 0
+B_PER = 1
+I_PER = 2
+B_LOC = 3
+I_LOC = 4
+B_ORG = 5
+I_ORG = 6
+B_MISC = 7
+I_MISC = 8
+START = 9
+
 def read_file(filename):
     stringInFile = ""
     with open(filename, "r", encoding="utf8") as fileObject:
         stringInFile = fileObject.read()
     return stringInFile
+
+
+def read_all_lines(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+    # remove whitespace characters like `\n` at the end of each line
+    lines = [line.strip() for line in lines]
+    return lines
+
+
+def read_tag_lines(filename):
+    lines = read_all_lines(filename)
+    desired_lines = lines[2::3]
+    return desired_lines
+
+
+def read_token_lines(filename):
+    lines = read_all_lines(filename)
+    desired_lines = lines[0::3]
+    return desired_lines
 
 
 def writeToCSVFile(rowList):
@@ -20,14 +52,35 @@ def writeToCSVFile(rowList):
             writer.writerow(string)
 
 
-def preprocess(some_text):
-    some_text = "<s> " + some_text
-    some_text = some_text.replace(" ’", "’")
-    some_text = some_text.replace("’ ", "’")
-    some_text = some_text.replace("\n", " ")
-    some_text = some_text.replace(" . ", " . </s> <s> ")
-    some_text = some_text[:-5]
-    return some_text
+def getTagId(tag_string):
+    if tag_string == "O": return 0
+    if tag_string == "B-PER": return 1
+    if tag_string == "I-PER": return 2
+    if tag_string == "B-LOC": return 3
+    if tag_string == "I-LOC": return 4
+    if tag_string == "B-ORG": return 5
+    if tag_string == "I-ORG": return 6
+    if tag_string == "B-MISC": return 7
+    if tag_string == "I-MISC": return 8
+
+
+# returns matrix M such that M[i, j] = # of (t_i t_j) sequences
+def createUnsmoothedTagBigramCounts(lines_of_tags):
+    bigramCountMatrix = np.zeros([10, 10])
+
+    for l in range(len(lines_of_tags)):
+        line = lines_of_tags[l].split(' ')
+        first_tag = getTagId(line[0])
+        bigramCountMatrix[START, first_tag] += 1
+        for i in range(1, len(l)):
+            bigramCountMatrix[getTagId(l[i-1]), getTagId(l[i])] += 1
+
+    return bigramCountMatrix
+
+
+def getBigramProbsFromCounts(bigramCountMatrix):
+    bigramProbMatrix = bigramCountMatrix / bigramCountMatrix.sum(axis=1, keepdims=True)
+    return bigramProbMatrix
 
 
 if __name__ == "__main__":
